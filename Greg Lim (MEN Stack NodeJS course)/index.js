bb90ced53f96ac8b9656b81c6d9de80193fd9e24
Heris <badairoinioluwa578@gmail.com>
2025-11-27 06:16:12 +0100

    #November 27 2025 (book)

--- Greg Lim (MEN Stack NodeJS course)/index.js
+++ Greg Lim (MEN Stack NodeJS course)/index.js
@@ -1,14 +1,25 @@
 import { createServer } from "node:http";
+import { readFileSync } from "node:fs";
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 8000;
+
+const homePage = readFileSync("./home.html", "utf-8");
+const contactPage = readFileSync("./contact.html", "utf-8");
+const aboutPage = readFileSync("./about.html");
+const notfoundPage = readFileSync("./notfound.html", "utf-8");
 
 const server = createServer((req, res) => {
-  console.log(req.url);
-  res.statusCode = 200;
-  res.setHeader("content-type", "text/plain");
-
-  res.end("Hello from index.js");
+  if (req.url === "/about") {
+    res.end(aboutPage);
+  } else if (req.url === "/contact") {
+    res.end(contactPage);
+  } else if (req.url === "/") {
+    res.end(homePage);
+  } else {
+    res.writeHead(404);
+    res.end(notfoundPage);
+  }
 });
 
 server.listen(port, hostname, () => {
